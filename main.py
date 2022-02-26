12826b89092b209545942e170d3e697fec70aac5
WellintonRafael <wellintonrafaelsantos@gmail.com>
2022-02-26 05:54:32 -0300

    deploy teste aplicação

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    return 'Em Breve teremos uma pesquisa de satisfação para melhor atender nossos clientes'
+    return render_template('homepage.html')
+
 
 @app.route('/login')
 def login():
     return 'login'
 
+
 @app.route('/user/<username>')
 def profile(username):
     return render_template('user.html', username=username)
 
-# with app.test_request_context():
-#     print(url_for('index'))
-#     print(url_for('login'))
-#     print(url_for('login', next='/'))
-#     print(url_for('profile', username='Jhon Doe'))
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
