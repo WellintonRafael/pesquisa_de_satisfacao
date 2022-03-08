4f8de162fcc75690f7f4cc4119492a70dbc215c9
WellintonRafael <wellintonrafaelsantos@gmail.com>
2022-03-08 13:16:59 -0300

    "Voltando ao normal"

--- static/js/main.js
+++ static/js/main.js
@@ -7,22 +7,28 @@ function enviarPesquisa() {
     var custo_beneficio = document.querySelectorAll('.custo_beneficio')
     var localizacao = document.querySelectorAll('.localizacao')
     var listaDeItens = [atendimento, limpeza, conforto, cafe, internet, custo_beneficio, localizacao]
-    var votacao = new Array()
+    var votos = new Array()
+
+    // Iterador para os RadioButtons.
+    // Acrescenta cada item selecionado na lista "votos"
     for (let contador1 = 0; contador1 < listaDeItens.length; contador1++) {
         for (let contador2 = 0; contador2 < listaDeItens[contador1].length; contador2++){
             if (listaDeItens[contador1][contador2].checked) {
-                votacao.push(`${listaDeItens[contador1][contador2].id}`)
+                votos.push(`${listaDeItens[contador1][contador2].id}`)
             }
         }
     }
+
     var regexAvaliacao = /(-ms|-s|-ps|-i)/g
     var listaItem = new Array()
     var listaAvaliacao = new Array()
-    for (var contRegex = 0; contRegex < votacao.length; contRegex++) {
-        var encontrarAv = votacao[contRegex].match(regexAvaliacao)
-        str = encontrarAv.toString()
-        repl = str.replace('-','')
-        listaAvaliacao.push(repl)
+
+    // Regex para encontrar os resultados das votações na lista "votos"
+    for (var contRegex = 0; contRegex < votos.length; contRegex++) {
+        var encontrarAv = votos[contRegex].match(regexAvaliacao)
+        strAv = encontrarAv.toString()
+        replace = strAv.replace('-','')
+        listaAvaliacao.push(replace)
     }
     window.alert(`${listaAvaliacao}`)
 }
